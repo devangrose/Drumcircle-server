2705ffbcf5c3f1dfa99b135f3d6df924828a5768
Devan Grose <devangrose@gmail.com>
2018-09-17 12:26:18 -0700

    added image field to user

--- models/user.js
+++ models/user.js
@@ -20,7 +20,8 @@ var userSchema = new mongoose.Schema({
     required: true,
     minlength: 6,
     maxlength: 99
-  }
+  },
+  image: String
 });
 
 // Override 'toJSON' to prevent the password from being returned with the user
